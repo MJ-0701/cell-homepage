72eb4f35d8c618aaf5d45114bd8f5422ed924c59
MJ-0701 <jack2718@naver.com>
2022-12-07 01:45:43 +0900

    init

---
+++ src/main/java/com/example/websevicedemo/WebSeviceDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.websevicedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSeviceDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSeviceDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/websevicedemo/WebSeviceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.websevicedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSeviceDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
